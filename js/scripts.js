218d70b2d4fe642be7eb3ca5a233b5099d473ebd
Ned Faulhaber <nedfaulhaber@gmail.com>
2016-12-02 19:32:35 -0800

    site is responsive to 412 px with a side push navbar from right

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function openNav() {
     document.getElementById("mobile-nav").style.width = "250px";
-    document.getElementById("wrapper").style.marginLeft = "250px";
+    document.getElementById("wrapper").style.marginRight = "250px";
 }
 
 function closeNav() {
     document.getElementById("mobile-nav").style.width = "0";
-    document.getElementById("wrapper").style.marginLeft= "0";
+    document.getElementById("wrapper").style.marginRight= "0";
 }
