229938267b0216618d64f7860ab99ff00f4cd213
Ned Faulhaber <nedfaulhaber@gmail.com>
2017-02-09 13:12:01 -0800

    made about page responsive, created contacts page, activated all links, finished project

---
+++ js/about-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("about-mobile-nav").style.width = "250px";
+    document.getElementById("wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("about-mobile-nav").style.width = "0";
+    document.getElementById("wrapper").style.marginRight= "0";
+}
